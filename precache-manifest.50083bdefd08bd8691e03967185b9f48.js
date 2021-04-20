c13e7eaff3f195ee782cf1acb94309a7c0e8f02a
debirizk <debirizky45@gmail.com>
2021-04-20 08:02:17 +0800

    Updates

---
+++ precache-manifest.50083bdefd08bd8691e03967185b9f48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0bd596c491eca5fb2f86c15c65bed4",
+    "url": "/covid-19-tracker-master/index.html"
+  },
+  {
+    "revision": "2748ec4c4a7d65ccdf29",
+    "url": "/covid-19-tracker-master/static/css/main.9a931f62.chunk.css"
+  },
+  {
+    "revision": "347e0111dc478f69cc1f",
+    "url": "/covid-19-tracker-master/static/js/2.26cc80bf.chunk.js"
+  },
+  {
+    "revision": "6ef57ea73f859e79d7f696b27898a6e5",
+    "url": "/covid-19-tracker-master/static/js/2.26cc80bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2748ec4c4a7d65ccdf29",
+    "url": "/covid-19-tracker-master/static/js/main.6ae8968a.chunk.js"
+  },
+  {
+    "revision": "c2ee3c2f1450c7fe1e69",
+    "url": "/covid-19-tracker-master/static/js/runtime-main.d726068c.js"
+  },
+  {
+    "revision": "b6753322240100d6050c6d610dc4ef41",
+    "url": "/covid-19-tracker-master/static/media/image.b6753322.png"
+  }
+]);
